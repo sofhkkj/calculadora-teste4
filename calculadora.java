064d1583a82533d662b896c1d275c8c7d4e9dc43
sof <130997583+sofhkkj@users.noreply.github.com>
2023-09-27 10:34:18 -0300

    Create calculadora.java

---
+++ calculadora.java
@@ -0,0 +1,49 @@
+package teste4;
+import java.util.Scanner;
+
+class adicao{
+	public static int add(int n1, int n2){
+		return n1 + n2;
+	}
+}
+class subtracao{
+	public static int add(int n1, int n2){
+		return n1 + n2;
+	}
+}
+class multiplicacao{
+	public static int add(int n1, int n2){
+		return n1 + n2;
+	}
+}
+class divisao{
+	public static int add(int n1, int n2){
+		return n1 + n2;
+	
+public class calculadora{
+	public static void main (String[] args) {
+		Scanner scan = new Scanner (System.in);	
+	
+int n1, n2, adicao, subtracao, multiplicacao, divisao;
+		
+		System.out.print("Informe o numero: ");
+		n1 = scan.nextInt();
+		
+		System.out.print("Informe o segundo numero: ");
+		n2 = scan.nextInt();
+		
+		adicao = n1 + n2;
+		System.out.println("A soma deu" + adicao);
+		
+		subtracao = n1 - n2;
+		System.out.println("A subtraçao deu" + subtracao);
+		
+		multiplicacao = n1 * n2;
+		System.out.println("A multiplicaçao deu" + multiplicacao);
+		
+		divisao = n1 / n2;
+		System.out.println("A divisao deu" + divisao);
+
+	}}
+	}
+		}
